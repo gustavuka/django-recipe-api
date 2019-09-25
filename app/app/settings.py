18319485d22e90f0db7acc0015ab806de96c1e63
Gustavo <go.gusfm@gmail.com>
2019-09-24 21:34:46 -0300

    Add working list tags endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'core_app',
     'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
---
+++ app/recipe/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from core_app.models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tag objects"""
+
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
